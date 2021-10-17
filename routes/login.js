1cfb1e2bb18b37ca42bdea5a732951c0f3b0a843
ChanannatM <chanannat10969@gmail.com>
2021-10-17 21:23:08 +0700

    login_regist

--- routes/login.js
+++ routes/login.js
@@ -15,24 +15,23 @@ db.connect(function (err) {
 const bodyparser = require('body-parser');
 
 router.post("/login_seller",function(req,res){
-   //รับค่าจาก input มาเก็บไว้ในตัวแปร
+   
   const email = req.body.email;
   const password = req.body.password;
 
-  //คำสั่ง sql เลือกข้อมูลจากตาราง
   const sql = "SELECT `seller`.`seller_id`,`seller`.`seller_name` FROM `seller` WHERE `seller_mail`= '" + email + "' AND `seller_password` = '" + password + "'"
   
   db.query(sql, function (err, result) { 
-    if(result.length>0){ //return ผลลัพธ์(result)จากตารางออกมา (ที่่กำหนดใน const sql)
-      res.send({status:1,name:result[0].seller_name,seller_id:result[0].seller_id}) //ถ้าข้อมูลถูกต้อง ส่งข้อมูลไป font 
-    }else res.send({status:0}) //ข้อมูลไม่ถูกต้อง ส่ง status 0 ไป font ให้ login ใหม่
+    if(result.length>0){ 
+      res.send({status:1,name:result[0].seller_name,seller_id:result[0].seller_id})
+    }else res.send({status:0}) 
   });
   
 
 })
 
 router.post("/regist_customer",function(req,res){
-  //รับค่าจาก input มาเก็บไว้ในตัวแปร
+
   const userName = req.body.userName;
   const email = req.body.email;
   const password = req.body.password;
@@ -41,7 +40,7 @@ router.post("/regist_customer",function(req,res){
   const sql = "SELECT `customer`.`customer_email` FROM `customer` WHERE `customer_email` =  '" + email + "'"
 
   db.query(sql, function (err, result) { 
-    if(result.length>0){ //ถ้า email ใน db ตรงกับที่รับ ให้ res เป็น 0
+    if(result.length>0){  
       res.send({status:0})
     }else{
       const sqlInsert = "INSERT INTO `customer`(`customer_id`, `customer_userName`, `customer_email`, `customer_phone_num`, `customer_password`) VALUES (NULL,'"+userName+"','"+email+"','"+password+"','"+phoneNum+"')"
